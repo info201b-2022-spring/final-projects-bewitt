2ea8ad973e95e3764a54848665d4b5ed1fc2aa04
jkamins27 <jkamins27>
2022-05-20 00:28:59 -0700

    Jackson pie chart update

---
+++ jacksontable.r
@@ -0,0 +1,24 @@
+# Relationship between Sex and Heart Disease
+# Jackson Kamins
+# 05/20/22
+# Group members: Jake Flynn, Jackson Kamins, Belle Witt, Daniel Kim
+
+#Loading Data
+library(tidyverse)
+library(dplyr)
+heart_df <- read.csv("/Users/jacksonkamins/Desktop/INFO 201/final-projects-bewitt/data/heart_2020_cleaned.csv")
+
+heart_df$HeartDisease <- ifelse(heart_df$HeartDisease == "Yes", 1,0)
+
+heart_disease_by_sex <- group_by(heart_df, Sex)
+
+sex_heart_disease <- summarise(heart_disease_by_sex, total_disease = sum(HeartDisease))
+
+
+#Create Pie Chart
+slices <- c(sex_heart_disease$total_disease)
+lbls <- c(slices)
+lbls <- paste(lbls, sex_heart_disease$Sex)
+lbls <- paste(lbls, "Deaths of Heart Disease")
+pie(slices, labels = lbls, main = "Pie Chart of Sex and Risk of Heart Disease")
+
