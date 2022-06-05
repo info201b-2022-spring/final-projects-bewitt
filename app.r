fecc01a9329b92b0c6ee89e6f5437a1bc8a70def
jakeflynn56 <flynnjake@icloud.com>
2022-06-05 15:57:29 -0700

    Jake

--- app.r
+++ app.r
@@ -3,16 +3,17 @@ library(ggplot2)
 library(plotly)
 
 # Load in data
-heart_data <- read.csv(url("https://raw.githubusercontent.com/info201b-2022-spring/final-projects-bewitt/main/data/heart_2020_cleaned.csv"))
+heart_data_og <- read.csv(url("https://raw.githubusercontent.com/info201b-2022-spring/final-projects-bewitt/main/data/heart_2020_cleaned.csv"))
+heart_data <- heart_data_og
 heart_data$HeartDisease<-ifelse(heart_data$HeartDisease=="Yes", 1, 0)
 categorical_data <- select(heart_data, Smoking, AlcoholDrinking, Stroke, DiffWalking, Sex, AgeCategory, Race, Diabetic, PhysicalActivity, GenHealth, Asthma, KidneyDisease, SkinCancer)
-continuous_data <- select(heart_data, BMI, PhysicalHealth, MentalHealth)
+continuous_data <- select(heart_data, BMI, PhysicalHealth, MentalHealth, SleepTime)
 
 intro_page <- tabPanel(
   "Introduction",
   titlePanel(strong("Heart Health in Adults in the United States")),
   p(em("Project Presented by Jake Flynn, Belle Witt & Jackson Kamins")),
-  img("heart image one", src = "image1.jpeg"),
+  img("Heart Image One", src = "image1.jpeg", height="50%", width="50%", align="center"),
   h2("Purpose of the Project"),
   p("For almost a century now, Heart Disease has been the leading cause of death in the United States. Living in a post
     pandemic world, all too many people have been faced with the unfortunate reality of losing a loved one. While there are still
@@ -21,7 +22,7 @@ intro_page <- tabPanel(
     project to help people better understand the factors that may put themseleves or their loved ones at risk for this disease. Two of the
     factors we were orginally most interested in seeing the effects of were sex and age, as well as the intersection of the two. Overall,
     we hope our analysis will give everyone a better understanding the risk fatcors associated with this disease."),
-  img("heart image two", src = "image2.jpeg"),
+  img("Heart Image Two", src = "image2.jpeg", height="50%", width="50%", align="center"),
   h2("About the Dataset"),
   p("This dataset was found on the Kaggle Website, and the data was sourced from the Center for Disease Control (CDC). The dataset contains
     information on 319,796 adults aged 55 years and older. It contains data from adults both with and without heart disease and reports
@@ -131,19 +132,14 @@ server <- function(input, output) {
       )
   })
   
-  make_hist_df <- function(var_name) {
-    hist_df_whd <- filter(heart_data, HeartDisease == 1)
-    hist_df_wohd <- filter(heart_data, HeartDisease == 1)
-  }
-  
   output$hist <- renderPlot({
-    ggplot(heart_data, aes(x = .data[[input$continuous]], fill = .data[[HeartDisease]], color = .data[[HeartDisease]])) +
-      geom_histogram(position = "identity")
+    ggplot(heart_data_og, aes(x = .data[[input$continuous]], color = HeartDisease, fill = HeartDisease)) +
+      geom_histogram(bins = 40, position = "identity")
   })
   
-  output$testing <- renderText({
-    paste0(input$continuous)
-  })
+  # output$testing <- renderText({
+    # paste0(heart_data$HeartDisease)
+  # })
   
 }
 
