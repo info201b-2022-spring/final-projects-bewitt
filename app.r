df6beeab5aca682c024dbbea719404ec572ab5f7
jakeflynn56 <flynnjake@icloud.com>
2022-06-05 15:19:26 -0700

    Jake Again.

--- app.r
+++ app.r
@@ -6,6 +6,7 @@ library(plotly)
 heart_data <- read.csv(url("https://raw.githubusercontent.com/info201b-2022-spring/final-projects-bewitt/main/data/heart_2020_cleaned.csv"))
 heart_data$HeartDisease<-ifelse(heart_data$HeartDisease=="Yes", 1, 0)
 categorical_data <- select(heart_data, Smoking, AlcoholDrinking, Stroke, DiffWalking, Sex, AgeCategory, Race, Diabetic, PhysicalActivity, GenHealth, Asthma, KidneyDisease, SkinCancer)
+continuous_data <- select(heart_data, BMI, PhysicalHealth, MentalHealth)
 
 intro_page <- tabPanel(
   "Introduction",
@@ -14,13 +15,13 @@ intro_page <- tabPanel(
   img("heart image one", src = "image1.jpeg"),
   h2("Purpose of the Project"),
   p("For almost a century now, Heart Disease has been the leading cause of death in the United States. Living in a post
-    pandemic world, all too many people have been faced with the unfortunate relaity of losing a loved one. While there are still
+    pandemic world, all too many people have been faced with the unfortunate reality of losing a loved one. While there are still
     many mysteries associated with heart disease and prevention measures, we hoped to take on this porject to learn more about the
     factors that may influence the prevalence of Heart Disease amongst the adult population of the United States. We aimed to use this
     project to help people better understand the factors that may put themseleves or their loved ones at risk for this disease. Two of the
     factors we were orginally most interested in seeing the effects of were sex and age, as well as the intersection of the two. Overall,
     we hope our analysis will give everyone a better understanding the risk fatcors associated with this disease."),
-  img("heart image two", src = url("https://github.com/info201b-2022-spring/final-projects-bewitt/blob/main/image%202.jpeg")),
+  img("heart image two", src = "image2.jpeg"),
   h2("About the Dataset"),
   p("This dataset was found on the Kaggle Website, and the data was sourced from the Center for Disease Control (CDC). The dataset contains
     information on 319,796 adults aged 55 years and older. It contains data from adults both with and without heart disease and reports
@@ -34,8 +35,26 @@ summary_page <- tabPanel(
   p("This website lets you explore Heart Health...")
 )
 
+cont_analysis_page <- tabPanel(
+  "Continuous Risk Factor Analysis",
+  sidebarLayout(
+    sidebarPanel(
+      h3("Control Panel"),
+      selectInput(
+        inputId = "continuous",
+        label = "Select a variable",
+        choices = colnames(continuous_data)
+      )
+    ),
+    mainPanel(
+      plotOutput(outputId ="hist"),
+      verbatimTextOutput(outputId = "testing")
+    )
+  )
+)
+
 rf_analysis_page <- tabPanel(
-  "Risk Factor Analysis",
+  "Categorical Risk Factor Analysis",
   sidebarLayout(
     sidebarPanel(
       h3("Control Panel"),
@@ -46,8 +65,7 @@ rf_analysis_page <- tabPanel(
       )
     ),
     mainPanel(
-      plotOutput(outputId = "blank"),
-      verbatimTextOutput(outputId = "test")
+      plotOutput(outputId = "blank")
     )
   )
 )
@@ -75,6 +93,7 @@ ui <- navbarPage(
   intro_page,
   cat_analysis_page,
   rf_analysis_page,
+  cont_analysis_page,
   summary_page
 )
 
@@ -86,12 +105,13 @@ server <- function(input, output) {
   }
   
   output$bar <- renderPlot({
-    ggplot(make_bar_df(input$var), aes(x = make_bar_df(input$var)[[as.name(input$var)]])) +
-      geom_bar(fill = "indianred3",
-               color = "black") +
+    ggplot(make_bar_df(input$var), aes(x = make_bar_df(input$var)[[as.name(input$var)]], fill = make_bar_df(input$var)[[as.name(input$var)]])) +
+      geom_bar() +
       labs(x = input$var,
            y = "Number of Participants",
-           title = paste("Participants by", input$var))
+           title = paste("Participants by", input$var),
+           fill = input$var) +
+      geom_text(aes(label = ..count..), stat = "count", vjust = -0.5)
   })
 
   make_grouped_df <- function(var_name) {
@@ -100,14 +120,31 @@ server <- function(input, output) {
   }
   
   output$blank <- renderPlot({
-    ggplot(make_grouped_df(input$variable), aes(x = make_grouped_df(input$variable)[[as.name(input$variable)]], y = heart_disease_prop)) +
-      geom_bar(stat = "identity",
-               fill = "indianred3",
-               color = "black") +
+    ggplot(make_grouped_df(input$variable), aes(x = make_grouped_df(input$variable)[[as.name(input$variable)]], y = heart_disease_prop, fill = make_grouped_df(input$variable)[[as.name(input$variable)]])) +
+      geom_bar(stat = "identity") +
       labs(x = input$variable,
            y = "Proportion",
-           title = paste("Proportion of Heart Disease by", input$variable))
+           title = paste("Proportion of Heart Disease by", input$variable),
+           fill = input$variable) +
+      geom_text(
+        aes(label = heart_disease_prop, vjust = -1)
+      )
   })
+  
+  make_hist_df <- function(var_name) {
+    hist_df_whd <- filter(heart_data, HeartDisease == 1)
+    hist_df_wohd <- filter(heart_data, HeartDisease == 1)
+  }
+  
+  output$hist <- renderPlot({
+    ggplot(heart_data, aes(x = .data[[input$continuous]], fill = .data[[HeartDisease]], color = .data[[HeartDisease]])) +
+      geom_histogram(position = "identity")
+  })
+  
+  output$testing <- renderText({
+    paste0(input$continuous)
+  })
+  
 }
 
 # Run the application
