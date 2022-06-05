bbe0f697764c15d703fbcade7a2c89d889df0d9d
bewitt <irqwitt@uw.edu>
2022-06-04 17:52:07 -0700

    Belle 6/4

--- app.r
+++ app.r
@@ -11,7 +11,7 @@ intro_page <- tabPanel(
   "Introduction",
   titlePanel(strong("Heart Health in Adults in the United States")),
   p(em("Project Presented by Jake Flynn, Belle Witt & Jackson Kamins")),
-  img("heart image one", src = "~/image1.jpeg"),
+  img("heart image one", src = "image1.jpeg"),
   h2("Purpose of the Project"),
   p("For almost a century now, Heart Disease has been the leading cause of death in the United States. Living in a post
     pandemic world, all too many people have been faced with the unfortunate relaity of losing a loved one. While there are still
@@ -20,8 +20,12 @@ intro_page <- tabPanel(
     project to help people better understand the factors that may put themseleves or their loved ones at risk for this disease. Two of the
     factors we were orginally most interested in seeing the effects of were sex and age, as well as the intersection of the two. Overall,
     we hope our analysis will give everyone a better understanding the risk fatcors associated with this disease."),
+  img("heart image two", src = url("https://github.com/info201b-2022-spring/final-projects-bewitt/blob/main/image%202.jpeg")),
   h2("About the Dataset"),
-  p("This website lets you explore Heart Health...")
+  p("This dataset was found on the Kaggle Website, and the data was sourced from the Center for Disease Control (CDC). The dataset contains
+    information on 319,796 adults aged 55 years and older. It contains data from adults both with and without heart disease and reports
+    factors such as Sex, Age, Race, and BMI, amongst others. This large amount of data allowed our project team to have a high
+    level of confidence that our findings could be applied to the greater US adult population. ")
 )
 
 summary_page <- tabPanel(
